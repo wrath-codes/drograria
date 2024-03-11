55f383f9837c8fe95b6eb2f162b6bf99f40b02fa
Raphael Vaz <raphael@wrath-codes.com>
2024-03-11 10:45:09 -0300

    feat(class): dao constructor
    Add constructor to GenericDAO in order to find out which class is being used

--- src/main/java/com/wrathcodes/restaurant/dao/GenericDAO.java
+++ src/main/java/com/wrathcodes/restaurant/dao/GenericDAO.java
@@ -1,11 +1,21 @@
 package com.wrathcodes.restaurant.dao;
 
+import java.lang.reflect.ParameterizedType;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.wrathcodes.restaurant.util.HibernateUtil;
 
 public class GenericDAO<Entity> {
+    private Class entityClass;
+
+    // constructor creation
+    @SuppressWarnings("unchecked")
+    public GenericDAO() {
+        this.entityClass = (Class<Entity>) ((ParameterizedType) getClass().getGenericSuperclass())
+                .getActualTypeArguments()[0];
+    }
+
     public void save(Entity entity) {
         // first we need to capture an open session
         Session session = HibernateUtil.getSessionFactory().openSession();
